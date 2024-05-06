071ae2d17e210e88ff1f1e2127004e26188a663f
Emiliano0X1 <egp2506@gmail.com>
2024-05-06 16:40:07 -0700

    classes

--- src/main/java/org/example/Mounstritos.java
+++ src/main/java/org/example/Mounstritos.java
@@ -1,17 +1,25 @@
 package org.example;
 
+import listas.ListaDE;
+
 public abstract class Mounstritos {
 
+    private String sprite;
     private String name;
     private int HP;
+    private int ID;
+    private ListaDE<movimientos> ataques;
     private int defense;
     private int ataque;
 
-    public Mounstritos(String name, int HP, int defense, int ataque) {
+    public Mounstritos(String name, int HP, int ID, ListaDE<movimientos> ataques, int defense, int ataque,String sprite) {
         this.name = name;
         this.HP = HP;
+        this.ID = ID;
+        this.ataques = ataques;
         this.defense = defense;
         this.ataque = ataque;
+        this.sprite = sprite;
     }
 
     public String getName() {
@@ -45,4 +53,6 @@ public abstract class Mounstritos {
     public void setAtaque(int ataque) {
         this.ataque = ataque;
     }
+
+    public abstract void AtaqueTipoEspecial();
 }

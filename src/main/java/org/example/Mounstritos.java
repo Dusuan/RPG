4ca6801f4afbfc62bb4861d058c4a725996ea168
Emiliano0X1 <egp2506@gmail.com>
2024-05-06 16:26:52 -0700

    classes

--- src/main/java/org/example/Mounstritos.java
+++ src/main/java/org/example/Mounstritos.java
@@ -1,4 +1,48 @@
 package org.example;
 
 public abstract class Mounstritos {
+
+    private String name;
+    private int HP;
+    private int defense;
+    private int ataque;
+
+    public Mounstritos(String name, int HP, int defense, int ataque) {
+        this.name = name;
+        this.HP = HP;
+        this.defense = defense;
+        this.ataque = ataque;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHP() {
+        return HP;
+    }
+
+    public int getDefense() {
+        return defense;
+    }
+
+    public int getAtaque() {
+        return ataque;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setHP(int HP) {
+        this.HP = HP;
+    }
+
+    public void setDefense(int defense) {
+        this.defense = defense;
+    }
+
+    public void setAtaque(int ataque) {
+        this.ataque = ataque;
+    }
 }

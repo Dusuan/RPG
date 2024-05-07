f63ff01e3f70ac15e4e74c6c475ac513e0d22bee
Ivan Mijares <ivan.f.mijares@gmail.com>
2024-05-06 17:00:57 -0700

    Listas

--- src/main/java/org/example/Duelo.java
+++ src/main/java/org/example/Duelo.java
@@ -3,9 +3,37 @@ package org.example;
 
 
 public class Duelo {
+    private int turno;
+    private Jugador jugador1;
+    private Jugador jugador2;
 
+    public Duelo(int turno, Jugador jugador1, Jugador jugador2) {
+        this.turno = turno;
+        this.jugador1 = jugador1;
+        this.jugador2 = jugador2;
+    }
 
+    public int getTurno() {
+        return turno;
+    }
 
+    public void setTurno(int turno) {
+        this.turno = turno;
+    }
 
+    public Jugador getJugador1() {
+        return jugador1;
+    }
 
+    public void setJugador1(Jugador jugador1) {
+        this.jugador1 = jugador1;
+    }
+
+    public Jugador getJugador2() {
+        return jugador2;
+    }
+
+    public void setJugador2(Jugador jugador2) {
+        this.jugador2 = jugador2;
+    }
 }
--- src/main/java/org/example/Mounstritos.java
+++ src/main/java/org/example/Mounstritos.java
@@ -1,25 +1,17 @@
 package org.example;
 
-import listas.ListaDE;
-
 public abstract class Mounstritos {
 
-    private String sprite;
     private String name;
     private int HP;
-    private int ID;
-    private ListaDE<Movimientos> ataques;
     private int defense;
     private int ataque;
 
-    public Mounstritos(String name, int HP, int ID, ListaDE<Movimientos> ataques, int defense, int ataque,String sprite) {
+    public Mounstritos(String name, int HP, int defense, int ataque) {
         this.name = name;
         this.HP = HP;
-        this.ID = ID;
-        this.ataques = ataques;
         this.defense = defense;
         this.ataque = ataque;
-        this.sprite = sprite;
     }
 
     public String getName() {
@@ -38,18 +30,6 @@ public abstract class Mounstritos {
         return ataque;
     }
 
-    public String getSprite() {
-        return sprite;
-    }
-
-    public int getID() {
-        return ID;
-    }
-
-    public ListaDE<Movimientos> getAtaques() {
-        return ataques;
-    }
-
     public void setName(String name) {
         this.name = name;
     }
@@ -65,18 +45,4 @@ public abstract class Mounstritos {
     public void setAtaque(int ataque) {
         this.ataque = ataque;
     }
-
-    public void setSprite(String sprite) {
-        this.sprite = sprite;
-    }
-
-    public void setID(int ID) {
-        this.ID = ID;
-    }
-
-    public void setAtaques(ListaDE<Movimientos> ataques) {
-        this.ataques = ataques;
-    }
-
-    public abstract void AtaqueTipoEspecial();
 }

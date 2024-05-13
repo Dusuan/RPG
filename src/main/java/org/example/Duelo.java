f1c2d79de5c467ffa95bbaf63ec9662bb8911e36
Ivan Mijares <ivan.f.mijares@gmail.com>
2024-05-13 16:43:58 -0700

    Hice funciones de busqueda del ataque actual, como el pokemon actual, para que el codigo se vea más limpio. Lo aplique e heal y en damage. Falta agregar la funcion de escudo en damge. Buff falta crearlo y aplicarlo en damage. falta debuff tambien

--- src/main/java/org/example/Duelo.java
+++ src/main/java/org/example/Duelo.java
@@ -1,9 +1,7 @@
 package org.example;
 
-import movimientos.Ataques;
-import movimientos.buff;
-import movimientos.Default;
-import movimientos.Healing;
+import movimientos.*;
+import pokemones.Mounstritos;
 
 
 public class Duelo {
@@ -16,6 +14,7 @@ public class Duelo {
         this.jugador1 = jugador1;
         this.jugador2 = jugador2;
     }
+
     public int getTurno() {
         return turno;
     }
@@ -40,105 +39,112 @@ public class Duelo {
         this.jugador2 = jugador2;
     }
 
-    public void movimientoPLayer1(int attk)
+    public Movimientos ataquePokemon1()
     {
-        try{
+        //funcion para encontrar el ataque seleccionado del pokemon actual
+        try
+        {
+            return jugador1.getListaMounstritos().obtener(0).getAtaques().obtener(0);
+        }
+        catch(Exception e)
+        {
+            System.out.println("Ha ocurrido un error en la busqueda del ataque del pokemon del jugador 1" + e.getMessage());
 
+        }
+        return null;
+    }
+    public Movimientos ataquePokemon2()
+    {
+        //funcion para encontrar el ataque seleccionado del pokemon actual
+        try
+        {
+            return jugador2
+                    .getListaMounstritos()
+                    .obtener(0)
+                    .getAtaques()
+                    .obtener(0);
+        }
+        catch(Exception e)
+        {
+            System.out.println("Ha ocurrido un error en la busqueda del ataque del pokemon del jugador 2" + e.getMessage());
+
+        }
+        return null;
+    }
+
+    public Mounstritos pokemonJugador2()
+    {
+        //funcion para encontrar al pokemon actual
+        try
+        {
+            return jugador2.getListaMounstritos().obtener(0);
+        }
+        catch(Exception e)
+        {
+            System.out.println("Ha ocurrido un error en la busqueda del pokemon del jugador 2" + e.getMessage());
+
+        }
+        return null;
+    }
+    public Mounstritos pokemonJugador1()
+    {
+        //funcion para encontrar al pokemon actual
+
+        try
+        {
+            return jugador1.getListaMounstritos().obtener(0);
+        }
+        catch(Exception e)
+        {
+            System.out.println("Ha ocurrido un error en la busqueda del pokemon del jugador 2" + e.getMessage());
+
+        }
+        return null;
+    }
+
+    public void movimientoPLayer1(int attk) {
+        try {
             //Damage
-            if(jugador1.getListaMounstritos().obtener(0).getAtaques().obtener(0) instanceof Ataques)
-            {
-                //Primero consigo la vida del pokemno activo del jugador 2,
-                //Del ataque consigo el daño que hace
-                //Multiplico el daño por la vida del pokemon, para que me de un porcentaje
-                jugador2
-                        .getListaMounstritos()
-                        .obtener(0)
-                        .setHP
-                                (
-                                        jugador2.getListaMounstritos()
-                                                .obtener(0)
-                                                .getHP() -
-                                                ((Ataques) jugador1
-                                                .getListaMounstritos()
-                                                .obtener(0)
-                                                .getAtaques()
-                                                .obtener(0))
-                                                .getDamage()*
-                                                        jugador2.getListaMounstritos()
-                                                        .obtener(0)
-                                                        .getMaxHP());
-                //checar los elementos para ver si hace mas damage
-                if(jugador1.getListaMounstritos().obtener(0).getAtaques().obtener(0).getTipo()== jugador2.getListaMounstritos().obtener(0).getDebilidades1() ||
-                        jugador1.getListaMounstritos().obtener(0).getAtaques().obtener(0).getTipo()== jugador2.getListaMounstritos().obtener(0).getDebilidades2())
+            if (ataquePokemon1() instanceof Ataques) {
+                //checar los elementos para ver si el ataque mas damage, sino hacer damage normal
+                if (ataquePokemon1().getTipo() == pokemonJugador2().getDebilidades1() ||
+                        ataquePokemon1().getTipo() == pokemonJugador2().getDebilidades2())
                 {
-                    double extraDamage=((Ataques) jugador1
-                            .getListaMounstritos()
-                            .obtener(0)
-                            .getAtaques()
-                            .obtener(0)
-                    )
-                            .getDamage()*.5;
-                    jugador2
-                            .getListaMounstritos()
-                            .obtener(0)
-                            .setHP
-                                    (jugador2.getListaMounstritos().obtener(0).getHP() - extraDamage);
-
+                    double extraDamage = ((Ataques) ataquePokemon1()).getDamage() * .5;
+                    pokemonJugador2().setHP(pokemonJugador2().getHP() - ((Ataques) ataquePokemon1()).getDamage() * pokemonJugador2().getMaxHP() +extraDamage);
                 }
-
-                    //checar si sigue vivo el pokemon
-                if(jugador2.getListaMounstritos().obtener(0).getHP()<=0)
+                else
                 {
-                    jugador2.getListaMounstritos()
-                            .obtener(0)
-                            .setVivo(false);
+                    //Primero consigo la vida del pokemno activo del jugador 2, del ataque consigo el daño que hace, Multiplico el daño por la vida del pokemon, para que me de un porcentaje
+                    pokemonJugador2().setHP(pokemonJugador2().getHP() - ((Ataques) ataquePokemon1()).getDamage() * pokemonJugador2().getMaxHP());
+                }
+                //checar si sigue vivo el pokemon
+                if (pokemonJugador2().getHP() <= 0) {
+                    pokemonJugador2().setVivo(false);
                 }
             }
 
             //Healing
-            else if(jugador1.getListaMounstritos().obtener(0).getAtaques().obtener(0) instanceof Healing)
-            {
-                //funcion para curar
-                // checo si esta al tope de su vida, si no esta no pasa nada
-                //y al no pasar nada, no se llama la funcion para cambio de turno
-                if(jugador1.getListaMounstritos().obtener(0).getHP()==jugador1.getListaMounstritos().obtener(0).getMaxHP())
+            else if (ataquePokemon1() instanceof Healing) {
+                //funcion para curar: checo si esta al tope de su vida, si no esta hago el heal
+                if (pokemonJugador1().getHP() < pokemonJugador1().getMaxHP())
                 {
-                    jugador1
-                            .getListaMounstritos()
-                            .obtener(0)
-                            .setHP
-                                    (
-                                            jugador1.getListaMounstritos()
-                                                    .obtener(0)
-                                                    .getHP() +
-                                                    ((Healing) jugador1
-                                                            .getListaMounstritos()
-                                                            .obtener(0)
-                                                            .getAtaques()
-                                                            .obtener(0))
-                                                            .getHeal());
+                    pokemonJugador1().setHP(pokemonJugador1().getHP() + ((Healing) ataquePokemon1()).getHeal());
 
-                    //checar si al sumar vida, esta es mayor a la vida max
-                    //si es, solo lo igualo a vida max y ya
-                    if((jugador1.getListaMounstritos().obtener(0).getHP() + ((Healing) jugador1.getListaMounstritos().obtener(0).getAtaques().obtener(0)).getHeal())>jugador1.getListaMounstritos().obtener(0).getMaxHP())
+                    //checar si al sumar vida, esta es mayor a la vida max, si es, solo lo igualo a vida max y ya
+                    if ((pokemonJugador1().getHP() + ((Healing) ataquePokemon1()).getHeal()) > pokemonJugador1().getMaxHP())
                     {
-                        jugador1
-                                .getListaMounstritos()
-                                .obtener(0)
-                                .setHP(jugador1
-                                        .getListaMounstritos()
-                                        .obtener(0)
-                                        .getMaxHP());
+                        pokemonJugador1().setHP(pokemonJugador1().getMaxHP());
                     }
                 }
             }
-            else if(jugador1.getListaMounstritos().obtener(0).getAtaques().obtener(0) instanceof buff)
+            else if (ataquePokemon1() instanceof buff)
             {
 
             }
 
         }
-        catch(Exception e)
+        catch (Exception e)
         {
             System.out.println("Ha ocurrido un error: relacionado con los movimientos" + e.getMessage());
 
@@ -147,11 +153,10 @@ public class Duelo {
     }
 
 
-
     public void movimientoPLayer2(int attk) // solo lo copie y cambie los jugadores
     {
-        try{
-            if(jugador2.getListaMounstritos().obtener(0).getAtaques().obtener(0) instanceof Ataques){
+        try {
+            if (jugador2.getListaMounstritos().obtener(0).getAtaques().obtener(0) instanceof Ataques) {
                 //Primero consigo la vida del pokemno activo del jugador 1,
                 //Del ataque consigo el daño que hace
                 //Multiplico el daño por la vida del pokemon, para que me de un porcentaje
@@ -168,21 +173,20 @@ public class Duelo {
                                                         .obtener(0)
                                                         .getAtaques()
                                                         .obtener(0))
-                                                        .getDamage()*
+                                                        .getDamage() *
                                                         jugador1.getListaMounstritos()
                                                                 .obtener(0)
                                                                 .getMaxHP());
                 //checar los elementos para ver si hace mas damage
-                if(jugador2.getListaMounstritos().obtener(0).getAtaques().obtener(0).getTipo()== jugador1.getListaMounstritos().obtener(0).getDebilidades1() ||
-                        jugador2.getListaMounstritos().obtener(0).getAtaques().obtener(0).getTipo()== jugador1.getListaMounstritos().obtener(0).getDebilidades2())
-                {
-                    double extraDamage=((Ataques) jugador2
+                if (jugador2.getListaMounstritos().obtener(0).getAtaques().obtener(0).getTipo() == jugador1.getListaMounstritos().obtener(0).getDebilidades1() ||
+                        jugador2.getListaMounstritos().obtener(0).getAtaques().obtener(0).getTipo() == jugador1.getListaMounstritos().obtener(0).getDebilidades2()) {
+                    double extraDamage = ((Ataques) jugador2
                             .getListaMounstritos()
                             .obtener(0)
                             .getAtaques()
                             .obtener(0)
                     )
-                            .getDamage()*.5;
+                            .getDamage() * .5;
                     jugador1
                             .getListaMounstritos()
                             .obtener(0)
@@ -192,14 +196,12 @@ public class Duelo {
                 }
 
                 //checar si sigue vivo el pokemon
-                if(jugador1.getListaMounstritos().obtener(0).getHP()<=0)
-                {
+                if (jugador1.getListaMounstritos().obtener(0).getHP() <= 0) {
                     jugador1.getListaMounstritos()
                             .obtener(0)
                             .setVivo(false);
                 }
-            }
-            else if( jugador2.getListaMounstritos().obtener(0).getAtaques().obtener(0) instanceof Healing){
+            } else if (jugador2.getListaMounstritos().obtener(0).getAtaques().obtener(0) instanceof Healing) {
                 jugador2
                         .getListaMounstritos()
                         .obtener(0)
@@ -215,9 +217,7 @@ public class Duelo {
                                                         .obtener(0))
                                                         .getHeal());
             }
-        }
-        catch(Exception e)
-        {
+        } catch (Exception e) {
             System.out.println("Ha ocurrido un error: relacionado con los movimientos" + e.getMessage());
 
         }
@@ -225,37 +225,34 @@ public class Duelo {
     }
 
 
-    public void cambioPoke1(int poke){
+    public void cambioPoke1(int poke) {
 
-        try{
-        if(jugador1.getListaMounstritos().obtener(poke).isVivo()){
+        try {
+            if (jugador1.getListaMounstritos().obtener(poke).isVivo()) {
                 jugador1
                         .getListaMounstritos()
                         .SwapPoke(poke); // implempente un metodo para cambiar dos nodos, no estoy seguro si funciona bien porque no es mi lista, pero segun yo deberia de jalar, REVISAR!
-        }
-        else{
-            System.out.println("Este pokemon ha sido derrotado!");
-        }
-        }catch(Exception e){
+            } else {
+                System.out.println("Este pokemon ha sido derrotado!");
+            }
+        } catch (Exception e) {
             System.out.println("Ocurrio un error relacionado con el cambio de pokemon");
         }
     }
 
-    public void cambioPoke2(int poke){
+    public void cambioPoke2(int poke) {
 
-        try{
-            if(jugador2.getListaMounstritos().obtener(poke).isVivo()){
+        try {
+            if (jugador2.getListaMounstritos().obtener(poke).isVivo()) {
                 jugador2
                         .getListaMounstritos()
                         .SwapPoke(poke); // implempente un metodo para cambiar dos nodos, no estoy seguro si funciona bien porque no es mi lista, pero segun yo deberia de jalar, REVISAR!
-            }
-            else{
+            } else {
                 System.out.println("Este pokemon ha sido derrotado!");
             }
-        }catch(Exception e){
+        } catch (Exception e) {
             System.out.println("Ocurrio un error relacionado con el cambio de pokemon");
         }
     }
-
-
 }
+

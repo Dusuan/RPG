179b4dbfc5fb39fc01d3ad9568e1d6942e88d0f8
Emiliano0X1 <egp2506@gmail.com>
2024-05-15 23:20:49 -0700

    Cree una clase nueva (Time) donde implemnte hilos para los turnos de los jugadores , por ejemplo si uno se queda AFK se pasa el turno al otro jugador y asi, puse los metodos que se ocupan para el cambio de turno, pero aun no se si funcione bien.

---
+++ src/main/java/org/example/Time.java
@@ -0,0 +1,56 @@
+package org.example;
+
+public class Time implements Runnable{
+
+    private Jugador player1;
+    private Jugador player2;
+    private long sleepTime; //Creo que estaria bien que fuera de 30 segundos o menos
+    private Jugador actualPlayer;
+    private boolean movimientoRealizado;
+
+
+    public Time(Jugador player1,Jugador player2,long sleepTime){ // Constructor para crear el tiempo de juego del jugador
+        this.player1 = player1;
+        this.sleepTime = sleepTime;
+        this.actualPlayer = player1; // Se iguala al primer jugador del turno
+        this.player2 = player2;
+        this.movimientoRealizado = false;
+    }
+
+    @Override
+    public void run() {
+            try {
+                Thread.sleep(sleepTime);
+                if(!movimientoRealizado){
+                    changeTurno(); // Cambia de turno automaticamente si no hizo nada
+                }
+                else{
+                    movimientoRealizado = false; // Se reinicia el hilo
+                }
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+    }
+
+    public void hizoMovimiento(){ // Este metodo sirve para poner que si ya se hizo el movimiento, aunque no se si se podra implmenetar bien,CHECK
+        movimientoRealizado = true;
+    }
+
+    public void changeTurno(){ //Este es el metdod0 que se encarga de cambiar de jugadores
+
+        if(actualPlayer.equals(player1)){
+            System.out.println("El tiempo de " + player1.getNombre() + " a terminado ");
+            System.out.println("Es turno del siguiente jugador");
+            actualPlayer = player2; // se iguala al jugador 2
+        }
+
+        else if(actualPlayer.equals(player2)){
+            System.out.println("El tiempo de " + player2.getNombre() + "a terminado");
+            System.out.println("Es turno del siguiente jugador");
+            actualPlayer = player1; // se iguala al jugador 1
+        }
+        else{
+            System.out.println("Aun tienes tiempo");
+        }
+    }
+}

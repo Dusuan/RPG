0c27fbaa47f6c1c00952d06ace51deadab397921
Ivan Mijares <ivan.f.mijares@gmail.com>
2024-06-04 03:46:54 -0700

    arreglue lo de las listas

--- src/main/java/listas/Interface.java
+++ src/main/java/listas/Interface.java
@@ -5,8 +5,8 @@ public interface Interface<T>
     public boolean empty();
     public int size();
     public void adicionar(T x);
-    public void insertar (T x, int posicion) throws Exception;
     public void eliminar (int posicion) throws Exception;
     public T obtener(int posicion) throws Exception;
+    public void insertar (T x, int posicion) throws Exception;
     public T buscar (T x) throws Exception; // es de tipo int o T
 }
